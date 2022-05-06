56df69c9abc71e2e5d5e47e48450d666b945ae50
Romildo Paiter <romildopaiter@gmail.com>
2022-05-06 08:41:58 -0300

    Initial Commit

---
+++ PromoCarnes/PromoCarnes.h
@@ -0,0 +1,18 @@
+//
+//  PromoCarnes.h
+//  PromoCarnes
+//
+//  Created by Romildo Jozue Paiter on 06/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PromoCarnes.
+FOUNDATION_EXPORT double PromoCarnesVersionNumber;
+
+//! Project version string for PromoCarnes.
+FOUNDATION_EXPORT const unsigned char PromoCarnesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PromoCarnes/PublicHeader.h>
+
+
