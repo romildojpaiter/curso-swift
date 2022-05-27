fd4dafd08e4d3a540884fe0a2dc7b1e0afb28ee1
Romildo Paiter <romildopaiter@gmail.com>
2022-05-26 21:45:52 -0300

    Atribuimos o Alamofire nas dependencias e criamos um Adapter

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,63 @@
+import XCTest
+
+import Alamofire
+
+class AlamofireAdapter {
+    
+    private let session: Session
+    
+    init(session: Session = .default){
+        self.session = session
+    }
+    
+    func post(to url: URL) {
+        session.request(url).resume()
+    }
+}
+
+class AlamofireAdapterTests: XCTestCase {
+
+    func test_() {
+        let url = makeUrl()
+        let configuration = URLSessionConfiguration.default
+        let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+        sut.post(to: url)
+        
+        // serve para fazer o teste aguarda
+        let exp = expectation(description: "waiting")
+        
+        UrlProtocolStub.observerRequest { request in
+            
+            XCTAssertEqual(url, request.url)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+
+}
+
+
+class UrlProtocolStub: URLProtocol {
+    
+    static var emit: ((URLRequest) -> Void)?
+    
+    static func observerRequest(completion: @escaping (URLRequest) -> Void) {
+        UrlProtocolStub.emit = completion
+    }
+    
+    
+    override open class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override open func startLoading() {
+        UrlProtocolStub.emit?(request)
+    }
+    
+    override open func stopLoading() {}
+}
